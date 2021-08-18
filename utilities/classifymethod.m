e362c4abf910793cbf0a291071ff5b4e489082a7
Xinwei Jiang <ysjxw@hotmail.com>
2021-08-18 16:06:25 +0800

    Update classifymethod.m

--- utilities/classifymethod.m
+++ utilities/classifymethod.m
@@ -1,6 +1,5 @@
 function [ correctRate,optC,optG, retYYSvm] = classifymethod(train_x,train_y,test_x,test_y )
-%UNTITLED 此处显示有关此函数的摘要
-%   此处显示详细说明
+
 %         addpath('./Tools');
 %         addpath('./svm2');      
         [accSvm, retYYSvm, optC, optG] = svmc(train_x, train_y, test_x, test_y, 0, 0, 1, 1);
